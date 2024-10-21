800a5d651e19ef7aecd8bf4da38422a47bfef307
Anand Prajapati <anandprajapati1282@gmail.com>
2024-10-21 21:03:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function openModal(src) {
+    document.getElementById("modalImage").src = src;
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
